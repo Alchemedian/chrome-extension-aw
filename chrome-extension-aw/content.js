05a8312a22455849160b86afb84fa335db00b27e
k0ck <k0ck@k0ck>
2020-08-11 13:57:12 +0100

    ukp search buttons added to profile page

--- chrome-extension-aw/content.js
+++ chrome-extension-aw/content.js
@@ -6,6 +6,52 @@ function isProfilePage() {
     return /ViewProfile.asp/.test(location.href)
 }
 
+function ukpSearchButtons(uid) {
+    let ukp = makeDiv('width:140px', '');
+    let ukpButtonReviewSearch = document.createElement('button');
+    ukpButtonReviewSearch.innerHTML = "UKP Reviews"
+    ukpButtonReviewSearch.style.padding = "1px"
+    ukpButtonReviewSearch.style.cursor = "pointer"
+    ukpButtonReviewSearch.addEventListener('click', () => {
+        event.preventDefault();
+        window.open("https://www.ukpunting.com/index.php?action=adultwork;id=" + uid);
+    })
+    ukp.appendChild(ukpButtonReviewSearch)
+
+    let dhid = document.createElement('div')
+    dhid.style.display = "none"
+    dhid.innerHTML = `
+            <form id="ku_ukp_form_${uid}" target="_blank" 
+            action="https://www.ukpunting.com/index.php?action=searchposts2"
+            method="post"
+            >
+            <input value="${uid}" name="query"/>
+            </form>
+            `
+    document.querySelector('body').after(dhid)
+    let ukpButton = document.createElement('button');
+    ukpButton.style.padding = "1px"
+    ukpButton.innerHTML = "UKP"
+    ukpButton.style.cursor = "pointer"
+    ukpButton.addEventListener('click', () => {
+        event.preventDefault();
+        document.getElementById(`ku_ukp_form_${uid}`).submit()
+    })
+
+    ukp.appendChild(ukpButton)
+    return ukp
+}
+
+function makeDiv(style, html, className) {
+    let div = document.createElement('div');
+    if (className) {
+        div.className = className
+    }
+    div.style = style;
+    div.innerHTML = html;
+    return div;
+}
+
 if (isSearchPage()) {
 
     //hide featured members crap
@@ -75,15 +121,7 @@ if (isSearchPage()) {
         return Object.keys(imgUniq)
     }
 
-    function makeDiv(style, html, className) {
-        let div = document.createElement('div');
-        if (className) {
-            div.className = className
-        }
-        div.style = style;
-        div.innerHTML = html;
-        return div;
-    }
+
 
     function showOverlayImage(uid, ord) {
         let eleOverlay = document.getElementsByClassName('pictureOverlay')[0]
@@ -316,39 +354,8 @@ if (isSearchPage()) {
             window.open(location.protocol + "//www.adultwork.com/ViewProfile.asp?UserID=" + uid);
             return false;
         };
-        let ukp = makeDiv('width:140px', '');
-        let ukpButtonReviewSearch = document.createElement('button');
-        ukpButtonReviewSearch.innerHTML = "UKP Reviews"
-        ukpButtonReviewSearch.style.padding = "1px"
-        ukpButtonReviewSearch.style.cursor = "pointer"
-        ukpButtonReviewSearch.addEventListener('click', () => {
-            event.preventDefault();
-            window.open("https://www.ukpunting.com/index.php?action=adultwork;id=" + uid);
-        })
-        ukp.appendChild(ukpButtonReviewSearch)
 
-        let dhid = document.createElement('div')
-        dhid.style.display = "none"
-        dhid.innerHTML = `
-            <form id="ku_ukp_form_${uid}" target="_blank" 
-            action="https://www.ukpunting.com/index.php?action=searchposts2"
-            method="post"
-            >
-            <input value="${uid}" name="query"/>
-            </form>
-            `
-        anchorTag.after(dhid)
-        let ukpButton = document.createElement('button');
-        ukpButton.style.padding = "1px"
-        ukpButton.innerHTML = "UKP"
-        ukpButton.style.cursor = "pointer"
-        ukpButton.addEventListener('click', () => {
-            event.preventDefault();
-            document.getElementById(`ku_ukp_form_${uid}`).submit()
-        })
-
-        ukp.appendChild(ukpButton)
-        anchorTag.after(ukp)
+        anchorTag.after(ukpSearchButtons(uid))
 
     })
     document.querySelectorAll('img.Border').forEach(function (x) {
@@ -391,31 +398,34 @@ if (isSearchPage() || isProfilePage()) {
 
     (function () {
         var parent = document.getElementById("stripMenuLevel2Container");
-        var child = document.createElement("div");
+        let topBar = document.createElement("div");
         let loc = location.href;
         if (loc.match(/UserID=([0-9]+)/) && loc.match(/UserID=([0-9]+)/)[1]) {
             loc = location.protocol + `//www.adultwork.com/UserID=` + loc.match(/UserID=([0-9]+)/)[1];
         } else if (loc.length > 80) {
             loc = loc.split('?')[0] + "?..."
         }
-        child.innerHTML = loc + "  &nbsp;&nbsp;" + String(new Date()).split(' ').slice(0, 4).join(' ');
-        child.id = "ku_top_bar"
-        child.style.border = "1px solid grey";
-        child.style.backgroundColor = "grey";
-        child.style.color = "white";
-        parent.after(child);
+        topBar.innerHTML = loc + "  &nbsp;&nbsp;" + String(new Date()).split(' ').slice(0, 4).join(' ');
+        topBar.id = "ku_top_bar"
+        topBar.style.border = "1px solid grey";
+        topBar.style.backgroundColor = "grey";
+        topBar.style.color = "white";
+        topBar.style.height = "22px";
+        topBar.style.lineHeight = "22px";
+        parent.after(topBar);
         let hidePhoneButton = isSearchPage() ?
-            `
+            `            
         <span style="margin-left:20px;height:18px" id='ku_hide'>
         <input id="ku_check_phone" type="checkbox" ${JSON.parse(window.localStorage.hideNoPhone) ? 'checked' : ''}/>
         <label style="font-size:10px;vertical-align:top;padding-top:3px;display:inline-block" for="ku_check_phone">Only show results with a phone</label></span>
         ` : '';
 
-        child.innerHTML += `${hidePhoneButton}
-    <div style="float: right;background-color: orange;font-size: 7pt;padding: 2px">    
+        topBar.innerHTML += `${hidePhoneButton}
+        <span id="ku_ukp_search"></span><div style="float: right;background-color: orange;font-size: 7pt;">    
     KUCK Vision</div>`
-        if (isSearchPage())
+        if (isSearchPage()) {
             document.getElementById('ku_hide').addEventListener('click', hideNoPhone)
+        }
     })();
 }
 
@@ -429,6 +439,12 @@ if (isSearchPage() || isProfilePage()) {
 
     let profileId = location.href.match(/UserID=([0-9]+)/)[1]
     let profileName = document.querySelector('.PageHeading').innerText
+
+    let ukpSearch = ukpSearchButtons(profileId)
+    ukpSearch.style.display = "inline"
+    ukpSearch.style.marginLeft = "10px"
+    ukpSearch.style.float = "right"
+    document.querySelector('#ku_ukp_search').append(ukpSearch)
 
     function removeLongTextualCrap() {
         let maxLen = 2000;
