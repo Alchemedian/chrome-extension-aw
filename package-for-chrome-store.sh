13374bd0d8719021f32abcfc0481ec66170a776a
k0ck <k0ck@k0ck>
2020-08-11 20:01:53 +0100

    updated zip

--- package-for-chrome-store.sh
+++ package-for-chrome-store.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm chrome-extension-aw.zip
 cd chrome-extension-aw
-zip -r chrome-extension-aw.zip * -x "*/.*"
+zip -r ../chrome-extension-aw.zip * -x "*/.*"
 git commit -am "updated zip"
