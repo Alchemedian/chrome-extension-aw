217d33771300068c59bbdf74a5f8223ff16f7b21
k0ck <k0ck@k0ck>
2020-08-15 19:42:19 +0100

    cache response from ukp api on cloudflare

--- etc/cloudflare-ukp-cors-proxy.js
+++ etc/cloudflare-ukp-cors-proxy.js
@@ -1,20 +1,35 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event))
 })
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
-async function handleRequest(request) {
+async function handleRequest(event) {
+    const request = event.request
     const url = new URL(request.url)
-    let apiUrl = url.searchParams.get('awid')
-    apiUrl = `https://www.ukpunting.com/aw2ukp.php?id=${apiUrl}`
-    let response = await fetch(apiUrl)
+    let awid = url.searchParams.get('awid')
+    let apiUrl = `https://www.ukpunting.com/aw2ukp.php?id=${awid}`
 
-    response = new Response(response.body, response)
-    // Set CORS headers
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    // Append to/Add Vary header so browser will cache response correctly
-    response.headers.append('Vary', 'Origin')
+    const cacheUrl = new URL(request.url)
+    const cacheKey = new Request(cacheUrl.toString(), request)
+    const cache = caches.default
+    // Get this request from this zone's cache
+    let response = await cache.match(cacheKey)
+    if (!response) {
+        response = await fetch(apiUrl)
+        response = new Response(response.body, response)
+        // Set CORS headers
+        response.headers.set('Access-Control-Allow-Origin', '*')
+        // Cache-Control: public
+        // response.headers.set('Cache-Control', 'public')
+        //max-age=<seconds>
+        response.headers.set('Cache-Control', ['public', 'max-age=604800'])
+        // Append to/Add Vary header so browser will cache response correctly
+        response.headers.append('Vary', 'Origin')
+        // await cache.put(cacheKey, response.clone())
+        event.waitUntil(cache.put(cacheKey, response.clone()))
+    }
+
     return response
 }
