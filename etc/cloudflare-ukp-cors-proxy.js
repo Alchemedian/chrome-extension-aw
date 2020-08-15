0de877e8ebc098e119c64d0ebafa87ac5048565a
k0ck <k0ck@k0ck>
2020-08-15 17:07:31 +0100

    added ukp cors proxy

---
+++ etc/cloudflare-ukp-cors-proxy.js
@@ -0,0 +1,20 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+    const url = new URL(request.url)
+    let apiUrl = url.searchParams.get('awid')
+    apiUrl = `https://www.ukpunting.com/aw2ukp.php?id=${apiUrl}`
+    let response = await fetch(apiUrl)
+
+    response = new Response(response.body, response)
+    // Set CORS headers
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    // Append to/Add Vary header so browser will cache response correctly
+    response.headers.append('Vary', 'Origin')
+    return response
+}
