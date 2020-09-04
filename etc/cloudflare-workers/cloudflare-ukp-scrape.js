16c37a0921a1d3de4045b940088366b2ea2f2392
k0ck <k0ck@k0ck>
2020-09-04 12:58:34 +0100

    cf worker - added order of reviews

--- etc/cloudflare-workers/cloudflare-ukp-scrape.js
+++ etc/cloudflare-workers/cloudflare-ukp-scrape.js
@@ -9,7 +9,7 @@ async function handleRequest(event) {
     const cacheUrl = new URL(request.url)
     const cacheKey = new Request(cacheUrl.toString(), request)
     const cache = caches.default
-    // Get this request from this zone's cache
+        // Get this request from this zone's cache
     let response = await cache.match(cacheKey)
     if (!response) {
         let awid = url.searchParams.get('awid')
@@ -27,20 +27,44 @@ async function handleRequest(event) {
             negative_count: negative,
             neutral_count: neutral,
             review_count: neutral + negative + positive,
+            order: getOrderOfReviews(text),
         }
 
         response = new Response(JSON.stringify(ret))
         response.headers.set('Content-Type', 'application/json');
         // Set CORS headers
         response.headers.set('Access-Control-Allow-Origin', '*')
-        // Cache-Control: public
-        // response.headers.set('Cache-Control', 'public')
-        //max-age=<seconds>
+            // Cache-Control: public
+            // response.headers.set('Cache-Control', 'public')
+            //max-age=<seconds>
         response.headers.set('Cache-Control', ['public', 'max-age=3600'])
-        // Append to/Add Vary header so browser will cache response correctly
+            // Append to/Add Vary header so browser will cache response correctly
         response.headers.append('Vary', 'Origin')
         event.waitUntil(cache.put(cacheKey, response.clone()))
     }
 
     return response
+}
+
+
+function getOrderOfReviews(content) {
+    return runningParser(content, ['positive.gif', 'negative.gif', 'neutral.gif'])
+        .reverse()
+        .map(ele => {
+            return ele
+                .replace('positive.gif', '👍')
+                .replace('negative.gif', '👎')
+                .replace('negative.gif', '😐')
+        })
+
+    function runningParser(haystack, needles) {
+        let ret = []
+        for (let i = 0; i < haystack.length; i++) {
+            needles.forEach(needle => {
+                if (haystack.substr(i, needle.length) === needle)
+                    ret.push(needle)
+            })
+        }
+        return ret
+    }
 }
--- package-for-chrome-store.sh
+++ package-for-chrome-store.sh
@@ -3,3 +3,15 @@ rm chrome-extension-aw.zip
 zip -r chrome-extension-aw.zip chrome-extension-aw -x "*/.*"
 git add chrome-extension-aw.zip
 git commit -m "updated zip"
+
+
+# CURTAG=`git describe --abbrev=0 --tags`;
+# IFS='.' read -a vers <<< "$CURTAG"
+# MAJ=${vers[0]}
+# MIN=${vers[1]}
+
+# echo "Current Tag: $MAJ.$MIN"
+# ((MIN+=1))
+# NEWTAG="$MAJ.$MIN"
+# echo "Adding Tag: $NEWTAG";
+# # git tag -a $NEWTAG -m $NEWTAG
