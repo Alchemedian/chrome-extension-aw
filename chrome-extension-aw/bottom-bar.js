78dbd0457da5a2b0cefcb7280777f64d6b67063e
k0ck <k0ck@k0ck>
2021-10-03 23:30:25 +0100

    1. cleanup 2. search cache by phone number added. 3. version bump

--- chrome-extension-aw/bottom-bar.js
+++ chrome-extension-aw/bottom-bar.js
@@ -1,11 +1,14 @@
 function bottomBar() {
     let awFooter = document.querySelector(".stripMenuLevelFooterContainer.menuOuterWrapper.mf-footer")
     let bBar = document.createElement('div')
+    const buttonSearch = "_ku_button_cache_search_tel"
     const buttonSave = "_ku_button_cache_save"
     const buttonRestore = "_ku_button_cache_restore"
     const fileInput = "_ku_cache_restore_file"
     bBar.innerHTML = `<div class="ku_bottom_bar">
 ${APP_NAME}:
+<button id="${buttonSearch}">Search cache for phone number</button>
+<span class="ku_spacer_with_vertical_line"></span>
 <button id="${buttonSave}">Save local cache to file</button>
 <button id="${buttonRestore}">Restore local cache from file</button>
 <input style="display:none" type="file" id="${fileInput}"><br>
@@ -13,6 +16,25 @@ ${APP_NAME}:
     </div>`
 
     awFooter.prepend(bBar)
+
+    document.getElementById(buttonSearch).addEventListener('click', () => {
+        let telNum = prompt(`Search ${APP_NAME} cache for a phone number`)
+        if (telNum) {
+            let telFull = telNum
+                .replace(/ /g, '')
+                .replace(/^0/, '+44')
+                .replace(/^/, '+')
+                .replace(/^\++/, '+')
+            let matches = findProfilesByPhoneNumber(telFull)
+            if (matches) {
+                alert(telFull + " matched: " + Object.keys(matches).join(", "))
+            } else {
+                alert(`No matches found for ${telFull}`)
+            }
+        }
+    })
+
+
     document.getElementById(buttonSave).addEventListener('click', () => {
         let aTemp = document.createElement('a');
         aTemp.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(localStorage[LOCAL_STORAGE_KEY_NAME])));
---
+++ chrome-extension-aw/constants.js
@@ -0,0 +1,20 @@
+const APP_NAME = "AW Civilizer"
+
+const LOCAL_STORAGE_KEY_NAME = '_ku_data'
+
+const ACRONYM_TO_SERVICE_REGEX = {
+    'OWO': [/Oral without Protection\n/, "😋"],
+    'CIM': [/CIM/, "👄"],
+    'Swallow': [/Swallow/, "💊"],
+    'Anal': [/"A"Levels\n/, "🍩"],
+    'DFK': [/French Kissing\n/, "😘"],
+    'Foot Worship': [/Foot Worship/, "👣"],
+    'Rimming': [/Rimming \(giving\)/, "👅"],
+    'Massage': [/Massage/, "💆‍♂️"],
+    'HR': [/Hand Relief/, "✊"],
+    'Strap On': [/Strap On/, "👺"],
+    'WS': [/Watersports/, "🏄"],
+    'BB': [/(Bareback|Unprotected Sex)/, "🤮"],
+    'DT': [/Deep Throat/, "🧕"],
+    'Tie & Tease': [/Tie & Tease/, "✝️"],
+};
