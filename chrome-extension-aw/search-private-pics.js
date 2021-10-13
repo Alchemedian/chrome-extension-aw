b286013bde9b2ddbfe0d204404c432574646b84c
k0ck <k0ck@k0ck>
2021-10-13 11:44:22 +0100

    useless style tweeks

--- chrome-extension-aw/search-private-pics.js
+++ chrome-extension-aw/search-private-pics.js
@@ -5,23 +5,23 @@ function isPicSearchPage() {
 if (isPicSearchPage()) {
     let elements = document.querySelectorAll("#tbl1Content tbody > tr > td table > tbody > tr > td a[title*='Click to view']");
     elements.forEach(ele => {
-        var id = ele.href.replace("javascript:sG('", "").replace("')", "");
-        replaceId(ele, id, "86", "18");
-        replaceId(ele, id, "46", "68");
-        replaceId(ele, id, "94", "08");
-        replaceId(ele, id, "78", "28");
-        replaceId(ele, id, "30", "88");
-        replaceId(ele, id, "54", "58");
-        replaceId(ele, id, "70", "38");
-        replaceId(ele, id, "38", "78");
+        let id = ele.href.replace("javascript:sG('", "").replace("')", "");
+        replaceIdSearchPicturesPage(ele, id, "86", "18");
+        replaceIdSearchPicturesPage(ele, id, "46", "68");
+        replaceIdSearchPicturesPage(ele, id, "94", "08");
+        replaceIdSearchPicturesPage(ele, id, "78", "28");
+        replaceIdSearchPicturesPage(ele, id, "30", "88");
+        replaceIdSearchPicturesPage(ele, id, "54", "58");
+        replaceIdSearchPicturesPage(ele, id, "70", "38");
+        replaceIdSearchPicturesPage(ele, id, "38", "78");
     })
 }
 
-function replaceId(ele, id, repeatnum, lastnum) {
+function replaceIdSearchPicturesPage(ele, id, repeatnum, lastnum) {
     function replaceAt(str, index, replacement) {
         return str.substr(0, index) + replacement + str.substr(index + replacement.length)
     }
-    var skip = 2;
+    let skip = 2;
     id = replaceAt(id, skip, repeatnum);
     id = replaceAt(id, skip * 3, repeatnum);
     id = replaceAt(id, skip * 5, repeatnum);
@@ -33,7 +33,7 @@ function replaceId(ele, id, repeatnum, lastnum) {
     id = replaceAt(id, skip * 15, repeatnum);
     id = replaceAt(id, id.length - 2, lastnum);
 
-    var url = `https://content.adultwork.com/cx/PG17/O/${id}.jpg`;
+    let url = `https://content.adultwork.com/cx/PG17/O/${id}.jpg`;
     let image = new Image();
     image.height = 100;
     image.style = "cursor:pointer";
