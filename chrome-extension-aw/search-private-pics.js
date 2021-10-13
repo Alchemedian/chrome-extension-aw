a92933c9f4d15fe96cc7374cd519c46862fb1406
k0ck <k0ck@k0ck>
2021-10-13 11:22:28 +0100

    style tweeks to pull request for searchprivate pics

--- chrome-extension-aw/search-private-pics.js
+++ chrome-extension-aw/search-private-pics.js
@@ -1,13 +1,12 @@
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);}
-
+function isPicSearchPage() {
+    return /SearchPictures.asp/i.test(location.href)
+}
 
 if (isPicSearchPage()) {
     let elements = document.querySelectorAll("#tbl1Content tbody > tr > td table > tbody > tr > td a[title*='Click to view']");
     elements.forEach(ele => {
-        var id = ele.href.replace("javascript:sG('","").replace("')","");
-
-        replaceId(ele,id,"86", "18");
+        var id = ele.href.replace("javascript:sG('", "").replace("')", "");
+        replaceId(ele, id, "86", "18");
         replaceId(ele, id, "46", "68");
         replaceId(ele, id, "94", "08");
         replaceId(ele, id, "78", "28");
@@ -18,23 +17,21 @@ if (isPicSearchPage()) {
     })
 }
 
-function isPicSearchPage() {
-    return /SearchPictures.asp/i.test(location.href)
-}
-
-function replaceId(ele,id, repeatnum, lastnum) {
+function replaceId(ele, id, repeatnum, lastnum) {
+    function replaceAt(str, index, replacement) {
+        return str.substr(0, index) + replacement + str.substr(index + replacement.length)
+    }
     var skip = 2;
-    id = id.replaceAt(skip, repeatnum);
-    id = id.replaceAt(skip * 3, repeatnum);
-    id = id.replaceAt(skip * 5, repeatnum);
-    id = id.replaceAt(skip * 5, repeatnum);
-    id = id.replaceAt(skip * 7, repeatnum);
-    id = id.replaceAt(skip * 9, repeatnum);
-    id = id.replaceAt(skip * 11, repeatnum);
-    id = id.replaceAt(skip * 13, repeatnum);
-    id = id.replaceAt(skip * 15, repeatnum);
-    id = id.replaceAt(id.length - 2, lastnum);
-
+    id = replaceAt(id, skip, repeatnum);
+    id = replaceAt(id, skip * 3, repeatnum);
+    id = replaceAt(id, skip * 5, repeatnum);
+    id = replaceAt(id, skip * 5, repeatnum);
+    id = replaceAt(id, skip * 7, repeatnum);
+    id = replaceAt(id, skip * 9, repeatnum);
+    id = replaceAt(id, skip * 11, repeatnum);
+    id = replaceAt(id, skip * 13, repeatnum);
+    id = replaceAt(id, skip * 15, repeatnum);
+    id = replaceAt(id, id.length - 2, lastnum);
 
     var url = `https://content.adultwork.com/cx/PG17/O/${id}.jpg`;
     let image = new Image();
@@ -42,16 +39,13 @@ function replaceId(ele,id, repeatnum, lastnum) {
     image.style = "cursor:pointer";
     image.src = url;
     image.onload = () => {
-        console.log(image.naturalWidth)
         if (image.naturalWidth < 2) {
             image.remove();
         }
     };
-    image.onclick =() =>{
+    image.onclick = () => {
         window.open(image.src)
     };
 
     ele.parentElement.parentElement.before(image);
 }
-
-
